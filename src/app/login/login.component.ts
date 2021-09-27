49fbf002d3a8f0bb037e6b96235e8fef77551c8f
Jhere-dev <jhere.dev@gmail.com>
2021-09-27 22:44:19 +0200

    signup maquetado y ngif funcionando

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  viewSignup :boolean = true
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+
+  changeViewClick(){
+    this.viewSignup = !this.viewSignup
+  }
 }
