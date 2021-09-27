3386c7cf1c49c449342d0cd922cf2b880a9107e1
Jhere-dev <jhere.dev@gmail.com>
2021-09-27 19:10:08 +0200

    login maquetado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { LoginViewComponent } from './login/login-view/login-view.component';
+import { SignupViewComponent } from './login/signup-view/signup-view.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    LoginViewComponent,
+    SignupViewComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
